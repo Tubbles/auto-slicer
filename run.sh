3c9c63e369b4b8eebf89052e83510114bee0be19
Tubbles <jae91m@gmail.com>
2023-10-11 19:04:22 +0200

    Add settings

---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+set -e
+
+my_dir="$(dirname "$(realpath "${0}")")"
+
+inputs="$(find "${my_dir}/" -type f -name '*.stl')"
+for input in ${inputs}; do
+    input_relative="$(realpath --relative-to="${my_dir}" "${input}")"
+
+    # Check if output file already exists
+    if compgen -G "${input/%.stl/}"*.gcode &>/dev/null; then
+        continue
+    fi
+
+    source "${my_dir}/config.sh"
+
+    flags=()
+    flags+=(--output-filename-format "{input_filename_base}_{print_time}_{temperature[0]}C_{filament_type[0]}.gcode")
+    flags+=(--load "${printer_dir}/${printer}")
+
+    # Fetch print
+    layer_height="$(echo "${input_relative}" | awk -F'/' '{print $1}')"
+    flags+=(--load "${print_dir}/${layer_heights["${layer_height}"]}")
+
+    # Fetch material
+    material="$(echo "${input_relative}" | awk -F'/' '{print $2}')"
+    flags+=(--load "${filament_dir}/${materials["${material}"]}")
+
+    # Do the slicing
+    (
+        set -x
+        "${slicer}" --export-gcode "${flags[@]}" "${input}" && while ! compgen -G "${input/%.stl/}"*.gcode &>/dev/null; do sleep 1; done
+    )
+
+    # Upload to printer
+done
